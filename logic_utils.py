8f62e6b29947c7cdb237733fe09173f7b192f8f8
Balamathangisuresh <mathangi137@gmail.com>
2026-06-18 18:28:34 -0500

    Tested pytest tests

--- logic_utils.py
+++ logic_utils.py
@@ -1,6 +1,14 @@
 def get_range_for_difficulty(difficulty: str):
     """Return (low, high) inclusive range for a given difficulty."""
-    raise NotImplementedError("Refactor this function from app.py into logic_utils.py")
+    # FIX: Changed logic so that the difficulty matches the range of numbers.
+    if difficulty == "Easy":
+        return 1, 20
+    if difficulty == "Normal":
+        return 1, 50
+    if difficulty == "Hard":
+        return 1, 100
+    return 1, 100
+
 
 
 def parse_guess(raw: str):
@@ -18,7 +26,24 @@ def check_guess(guess, secret):
 
     outcome examples: "Win", "Too High", "Too Low"
     """
-    raise NotImplementedError("Refactor this function from app.py into logic_utils.py")
+    if guess == secret:
+        return "Win", "🎉 Correct!"
+#FIX: Hint is reversed. If guess is higher than secret, it should say "Go LOWER!" and vice versa.
+    try:
+        if guess > secret:
+            return "Too High", "📉 Go LOWER!"
+        else:
+            return "Too Low", "📈 Go HIGHER!"
+    except TypeError:
+        g = str(guess)
+        if g == secret:
+            return "Win", "🎉 Correct!"
+        #FIX: Type Casts the strings to ints before comparing
+        if int(g) > int(secret):
+            return "Too High", "📉 Go LOWER!"
+        else:
+            return "Too Low", "📈 Go HIGHER!"
+
 
 
 def update_score(current_score: int, outcome: str, attempt_number: int):
